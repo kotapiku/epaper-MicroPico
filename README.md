ff0aca3227d2713f7ed2816cf218f381ef31e68b
kotapiku <ice08235523@gmail.com>
2024-05-08 23:08:16 +0900

    make icons

---
+++ README.md
@@ -0,0 +1,9 @@
+# raspberry pi pico + e-paper
+
+## License
+
+![CC](https://licensebuttons.net/l/by-sa/3.0/88x31.png)
+
+**[Creative Commons Attribution-ShareAlike](https://creativecommons.org/licenses/by-sa/4.0/)**
+
+**CC BY-SA**
--- images/icon.py
+++ images/icon.py
@@ -1,34 +1,56 @@
 import sys
+import os
+import re
 from PIL import Image
 
-def color(t) -> str:
-    r, g, b = t
-    if r >= 180:
-        return '2'
-    elif (r+g+b)/3 >= 50:
-        return '1'
-    return '0'
+def xbm_to_bitstring(xbm_file_path: str, width: int, height: int):
+    if os.path.splitext(xbm_file_path)[1] != 'xbm':
+        print(f"{xbm_file_path} is not a xbm file")
+        return []
+    with open(xbm_file_path, 'r') as file:
+        xbm_data = file.read()
 
-# python3 get_list_from_png.py 01d.png 16 16
+    # ビットマップデータを抽出
+    bits_data = re.search(r'static\s+char\s+\w+_bits\[\]\s+=\s+\{([^}]+)\};', xbm_data, re.DOTALL).group(1)
+    bits_data = bits_data.replace('\n', '').replace(' ', '').split(',')
+
+    # ビット列を01に変換
+    bitstring = ''
+    for byte in bits_data:
+        byte = byte.strip()
+        if byte:
+            bitstring += f'{int(byte, 16):08b}'
+
+    half = width // 2
+    bit_rows = []
+    for i in range(height):
+        bit_rows.append(bitstring[i*width+half:(i+1)*width] + bitstring[i*width:i*width+half])
+
+    return bit_rows
+
+def write_to_file(path, width, height):
+        rows = xbm_to_bitstring(path, width, height)
+        for row in rows:
+            print(row)
+
+        file_name = f"{os.path.splitext(os.path.basename(path)[:-4])[0]}_{width}_{height}.text"
+        with open(file_name, 'w') as f:
+            f.write("\n".join(rows))
+            print(f"succeeded to write to {file_name}")
+
+# python3 icon.py weather-pixel-icons/16/sum.xbm 16 16
+# -> translate the input xbm file to sum_16_16.txt
 if __name__ == "__main__":
     args = sys.argv
     if len(args) == 1:
         print("please specify paths to images")
 
-    png = args[1]
+    path = args[1]
     width = int(args[2])
     height = int(args[3])
 
-    white = (0, 0, 0) # 0
-    black = (73, 72, 74) # 1
-    red = (236, 110, 76) # 2
-    with Image.open(png) as im:
-        resized = im.resize((width, height), resample=Image.BILINEAR)
-        ret = list(resized.getdata())
-        for i in range(len(ret)):
-            ret[i] = color(ret[i][:3])
-        ret = [" ".join([ret[i+j*width] for i in range(width)]) for j in range(height)]
-        file_name = png[:-4] + ".txt"
-        with open(file_name, 'w') as f:
-            f.write("\n".join(ret))
-        print(f"succeeded to write to {file_name}")
+    if os.path.isfile(path):
+        write_to_file(path, width, height)
+    elif os.path.isdir(path):
+        for file in os.listdir(path):
+            write_to_file(os.path.join(path, file), width, height)
