6d934985bbb16bfd81c6896535b4b71bfd461780
kotapiku <ice08235523@gmail.com>
2024-05-14 21:58:44 +0900

    format

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from secret import SSID, PASSWORD, OWM_API_KEY, PERSON0, PERSON1
 ## wlan and time
 ##
 
+
 def connect_wlan_and_settime(ssid: str, password: str, host: str = "ntp.nict.jp"):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -28,16 +29,17 @@ def connect_wlan_and_settime(ssid: str, password: str, host: str = "ntp.nict.jp"
 
     ntptime.host = host
 
-    for _ in range(0,10):
+    for _ in range(0, 10):
         try:
             print("settime")
             ntptime.settime()
         except OSError as e:
-            if e.errno == 110: # when ETIMEDOUT
+            if e.errno == 110:  # when ETIMEDOUT
                 continue
             else:
                 raise
         break
+
 
 def disconnect_wlan():
     wlan = network.WLAN(network.STA_IF)
@@ -45,25 +47,30 @@ def disconnect_wlan():
     wlan.deinit()
     print("Network connected:", wlan.isconnected())
 
+
 def local_date_time_getter(offset_min: int = 9 * 60) -> time.struct_time:
     return time.localtime(time.time() + offset_min * 60)
 
+
 def draw_date_and_time(epd: EPD_7in5_B):
-    (year, month, day, hour, min, _, weekday_num,
-         _) = local_date_time_getter()
+    (year, month, day, hour, min, _, weekday_num, _) = local_date_time_getter()
 
     weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    date_string = f"{year}-{month:02d}-{day:02d} ({weekdays[weekday_num]}) {hour:02d}:{min:02d}"
+    date_string = (
+        f"{year}-{month:02d}-{day:02d} ({weekdays[weekday_num]}) {hour:02d}:{min:02d}"
+    )
     print("succeeded to get date and time: ", date_string)
 
-    epd.imageblack.large_text(date_string, 5, 10, 4, 0x00) # s, x, y, m, c
+    epd.imageblack.large_text(date_string, 5, 10, 4, 0x00)  # s, x, y, m, c
+
 
 ##
 ## weather
 ##
 
+
 class OpenWeatherMap:
-    def __init__(self, city, units='metric', lang='en'):
+    def __init__(self, city, units="metric", lang="en"):
         self.city = city
         self.units = units
         self.lang = lang
@@ -72,13 +79,13 @@ class OpenWeatherMap:
         try:
             url = f"https://api.openweathermap.org/geo/1.0/direct?q={city}&appid={OWM_API_KEY}"
             print("request: ", url)
-            res = urequests.post(url).json() # TODO: 405 error occur
+            res = urequests.post(url).json()  # TODO: 405 error occur
             print("geo info: ", res)
 
-            self.lat = res[0].get('lat')
-            self.lon = res[0].get('lon')
+            self.lat = res[0].get("lat")
+            self.lon = res[0].get("lon")
         except:
-            self.lat = 35.6828387 # Tokyo
+            self.lat = 35.6828387  # Tokyo
             self.lon = 139.7594549
         print("lat: ", self.lat, "lon: ", self.lon)
 
@@ -98,56 +105,61 @@ class OpenWeatherMap:
 
         return res
 
-celsius = u"\u00b0" + "C"
+
+celsius = "\u00b0" + "C"
+
 
 def draw_icon(name: str, epd: EPD_7in5_B, x: int, y: int, wh: int):
     print(f"draw icon: images/{translate_weather_icon(name, wh)}")
-    with open(f'images/{translate_weather_icon(name, wh)}', 'r') as f:
+    with open(f"images/{translate_weather_icon(name, wh)}", "r") as f:
         icon_rows = f.read().split()
         icon = [list(_) for _ in icon_rows]
         for i in range(len(icon)):
             for j in range(wh):
-                if icon[i][j] == '1':
-                    epd.imageblack.pixel(x+j, y+i, 0x00)
-                elif icon[i][j] == '2':
-                    epd.imagered.pixel(x+j, y+i, 0xff)
+                if icon[i][j] == "1":
+                    epd.imageblack.pixel(x + j, y + i, 0x00)
+                elif icon[i][j] == "2":
+                    epd.imagered.pixel(x + j, y + i, 0xFF)
+
 
 # wh = 16 or 32
 def translate_weather_icon(icon: str, wh: int) -> str:
     icon_map = {
-        '01d': 'sun',
-        '01n': 'moon',
-        '02d': 'cloud_sun',
-        '02n': 'cloud_moon',
-        '03d': 'cloud',
-        '03n': 'cloud',
-        '04d': 'clouds',
-        '04n': 'clouds',
-        '09d': 'rain0',
-        '09n': 'rain0',
-        '10d': 'rain1_sun',
-        '10n': 'rain1_moon',
-        '11d': 'lightning',
-        '11n': 'lightning',
-        '13d': 'snow',
-        '13n': 'snow',
-        '50d': 'mist',
-        '50n': 'mist',
+        "01d": "sun",
+        "01n": "moon",
+        "02d": "cloud_sun",
+        "02n": "cloud_moon",
+        "03d": "cloud",
+        "03n": "cloud",
+        "04d": "clouds",
+        "04n": "clouds",
+        "09d": "rain0",
+        "09n": "rain0",
+        "10d": "rain1_sun",
+        "10n": "rain1_moon",
+        "11d": "lightning",
+        "11n": "lightning",
+        "13d": "snow",
+        "13n": "snow",
+        "50d": "mist",
+        "50n": "mist",
     }
-    return f'{icon_map[icon]}_{wh}_{wh}.txt'
+    return f"{icon_map[icon]}_{wh}_{wh}.txt"
 
 
-def draw_weather(epd: EPD_7in5_B, owm:OpenWeatherMap):
+def draw_weather(epd: EPD_7in5_B, owm: OpenWeatherMap):
     weather_data = owm.get_current_weather()
-    temp_min = weather_data.get('main').get('temp_min')
-    temp_max = weather_data.get('main').get('temp_max')
-    temp = weather_data.get('main').get('temp')
-    feels_like = weather_data.get('main').get('feels_like')
-    weather_icon = weather_data.get('weather')[0].get('icon') # e.g. 02n
+    temp_min = weather_data.get("main").get("temp_min")
+    temp_max = weather_data.get("main").get("temp_max")
+    temp = weather_data.get("main").get("temp")
+    feels_like = weather_data.get("main").get("feels_like")
+    weather_icon = weather_data.get("weather")[0].get("icon")  # e.g. 02n
 
     draw_icon(weather_icon, epd, 15, 70, 32)
 
-    current_weather_string = f"{temp:.1f}{celsius} (feels like {feels_like:.1f}{celsius})"
+    current_weather_string = (
+        f"{temp:.1f}{celsius} (feels like {feels_like:.1f}{celsius})"
+    )
     temp_min_max_string = f"H:{temp_max:.1f}{celsius}  L:{temp_min:.1f}{celsius}"
 
     epd.imageblack.large_text(current_weather_string, 50, 80, 2, 0x00)
@@ -157,31 +169,34 @@ def draw_weather(epd: EPD_7in5_B, owm:OpenWeatherMap):
 # "2022-03-15 16:00:00" |-> "01" (UTC+9)
 def format_dt_txt(dt_txt: str) -> str:
     hour = dt_txt.split(" ")[1].split(":")[0]
-    return '{:02d}'.format((int(hour)+9) % 24)
+    return "{:02d}".format((int(hour) + 9) % 24)
 
-def draw_3hour_forecast_weather(epd: EPD_7in5_B, owm:OpenWeatherMap):
+
+def draw_3hour_forecast_weather(epd: EPD_7in5_B, owm: OpenWeatherMap):
     forecast_data = owm.get_3hour_forecast_data()
 
     print("draw forecasts")
-    for i, res in enumerate(forecast_data.get('list')):
-        x = i*150
+    for i, res in enumerate(forecast_data.get("list")):
+        x = i * 150
         if i != 0:
-            epd.imageblack.vline(x, 154, 32, 0x00) # x, y, h, c
+            epd.imageblack.vline(x, 154, 32, 0x00)  # x, y, h, c
 
-        temp = res.get('main').get('temp')
-        when = format_dt_txt(res.get('dt_txt'))
-        weather_icon = res.get('weather')[0].get('icon') # e.g. 02n
+        temp = res.get("main").get("temp")
+        when = format_dt_txt(res.get("dt_txt"))
+        weather_icon = res.get("weather")[0].get("icon")  # e.g. 02n
 
-        epd.imageblack.large_text(when, x+70, 154, 1, 0x00)
+        epd.imageblack.large_text(when, x + 70, 154, 1, 0x00)
 
-        draw_icon(weather_icon, epd, x+30, 154, 32)
+        draw_icon(weather_icon, epd, x + 30, 154, 32)
         weather_temp_string = f"{temp:.1f}{celsius}"
-        epd.imageblack.large_text(weather_temp_string, x+70, 170, 1, 0x00)
+        epd.imageblack.large_text(weather_temp_string, x + 70, 170, 1, 0x00)
+
 
 class BathInCharge:
     def __init__(self):
         self.date = local_date_time_getter()[2]
         self.person = 0
+
     def who_in_charge(self) -> int:
         date, hour = local_date_time_getter()[2:4]
         if date != self.date and hour > 4:
@@ -189,29 +204,31 @@ class BathInCharge:
             self.date = date
         return self.person
 
+
 ##
 ## bath
 ##
 
-def draw_bath_in_charge(epd: EPD_7in5_B, bic:BathInCharge):
+
+def draw_bath_in_charge(epd: EPD_7in5_B, bic: BathInCharge):
     print("draw who is in charge of boiling bath")
     person_string = [PERSON0, PERSON1][bic.who_in_charge()]
     epd.imageblack.large_text(f"bath: {person_string}", 15, 200, 2, 0x00)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     epd = EPD_7in5_B()
     epd.Clear()
 
     connect_wlan_and_settime(SSID, PASSWORD)
 
-    owm = OpenWeatherMap('Tokyo')
+    owm = OpenWeatherMap("Tokyo")
     bic = BathInCharge()
 
     try:
         SLEEP_MINUTES = 60
         while True:
-            epd.imageblack.fill(0xff)
+            epd.imageblack.fill(0xFF)
             epd.imagered.fill(0x00)
 
             print("---draw date and time---")
@@ -239,4 +256,4 @@ if __name__ == '__main__':
     except:
         print("sleep and reset")
         epd.sleep()
-        machine.reset() # to avoid memory allocation error
+        machine.reset()  # to avoid memory allocation error
